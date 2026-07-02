5155d93a49eab06885d93c32988d719bb6569dec
vishaldayalu12345-web <vishaldayalu12345@gmail.com>
2026-07-03 02:06:15 +0530

    Day 3: Added multilingual translation using GoogleTranslator

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, request
+from deep_translator import GoogleTranslator
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+
+    language_codes = {
+        "English": "en",
+        "Kannada": "kn",
+        "Telugu": "te",
+        "Marathi": "mr",
+        "Hindi": "hi",
+        "Punjabi": "pa"
+    }
 
     if request.method == 'POST':
 
@@ -11,13 +21,25 @@ def home():
         source_language = request.form['source_language']
         target_language = request.form['target_language']
 
+        # Create translator using language codes
+        translator = GoogleTranslator(
+            source=language_codes[source_language],
+            target=language_codes[target_language]
+        )
+
+        # Translate the text
+        translated_text = translator.translate(text)
+
+        # Send all data to HTML
         return render_template(
             'index.html',
             text=text,
             source_language=source_language,
-            target_language=target_language
+            target_language=target_language,
+            translated_text=translated_text
         )
 
+    # First time opening the page
     return render_template('index.html')
 
 if __name__ == '__main__':
